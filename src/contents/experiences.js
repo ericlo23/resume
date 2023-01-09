92e8119b0d09f0327c6a477df6f5f28025a4b8fe
Eric Lo <eric.lo@tranx.io>
2023-01-09 21:36:46 +0800

    feat: simplify experiences

--- src/contents/experiences.js
+++ src/contents/experiences.js
@@ -6,27 +6,27 @@ export const experiences = [
   {
     name: "Arasens Inc., Front-end Engineer, Apr. 2019 ~ Oct. 2019",
     location: "Taipei, Taiwan",
-    desc: `● Developed a service platform to monitor vessels, schedule maintenance events, configure and watch alert notifications, and provide statistics.
-    ● Built a mobile application to monitor a vessel, watch alert notifications independently.
+    desc: `● Developed a service platform to monitor vessels, schedule events, configure notifications, and provide statistics.
+    ● Built a mobile application to monitor a vessel and watch notifications independently.
     ● Built the corporate website from scratch.
     ● Maintain back-end services.`,
     subDesc: `Skills:
-    React, React Native, Gatsby, Redux, Redux Saga, D3, Google Maps, SEO, NodeJS, PostgreSQL, Sequelize, Serverless, AWS, Typescript, BLE, WebSocket, Git, Zeplin, Webpack, LESS`,
+    React, React Native, NodeJS, PostgreSQL, AWS, Git, Zeplin, Webpack, LESS`,
   },
   {
     name: "Contribulet Inc., Front-end Engineer, Oct. 2017 ~ Apr. 2019",
     location: "San Jose, U.S.A (Remote)",
-    desc: `● Built the corporate website to raise charity events, participate events, and sell/purchase/redeem vouchers.
-    ● Developed the management dashboard for fundraiser and merchants to organize events independently.
-    ● Built a mobile application for donors to purchase/redeem vouchers from scratch.
-    ● Developed a POS application for merchants to redeem vouchers from scratch.`,
+    desc: `● Developed the service platform to raise charity events and sell vouchers.
+    ● Developed the management dashboard for users to organize events.
+    ● Built a mobile application for donors to purchase vouchers from scratch.
+    ● Built a POS application for merchants to redeem vouchers from scratch.`,
     subDesc: `Skills:
-    React, Redux, Angular, RxJS, Cordova, Android, Nginx, PHP, SEO, Push Notification, Google Maps, Git, Zeplin, Webpack, SASS`,
+    React, Angular, Cordova, Git, Zeplin, Webpack, SASS`,
   },
   {
     name: "Dynacolor Inc, Software Engineer, Mar. 2017 ~ Sep. 2017",
     location: "Taipei, Taiwan",
-    desc: "● Developed a streaming server to encode and wrap video and audio and stream to RTSP client.",
+    desc: "● Developed a streaming server to encode media and stream to RTSP client.",
     subDesc: `Skills:
     C++, Streaming, RTSP, Linux`,
   },
