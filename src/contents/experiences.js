9d52632ecf76ca314e3d1367b874c503fd7ec5d3
Eric Lo <eric.lo@tranx.io>
2023-01-09 20:34:11 +0800

    fix: use proper abbreviations

--- src/contents/experiences.js
+++ src/contents/experiences.js
@@ -1,17 +1,17 @@
 export const experiences = [
   {
-    name: "aetherAI Co. Ltd., Front-end Engineer, Apr 2020 ~",
+    name: "aetherAI Co. Ltd., Front-end Engineer, Apr. 2020 ~",
     location: "Taipei, Taiwan",
   },
   {
-    name: "New Garden Co. Ltd., Front-end Engineer, Dec 2019 ~ Dec 2019",
+    name: "New Garden Co. Ltd., Front-end Engineer, Dec. 2019 ~ Dec. 2019",
     location: "Taipei, Taiwan",
     desc: "● Developed a CMS platform.",
     subDesc: `Skills:
     React, Redux, Firebase, SASS`,
   },
   {
-    name: "Arasens Inc., Front-end Engineer, Apr 2019 ~ Oct 2019",
+    name: "Arasens Inc., Front-end Engineer, Apr. 2019 ~ Oct. 2019",
     location: "Taipei, Taiwan",
     desc: `● Developed a service platform to monitor vessels, schedule maintenance events, configure and watch alert notifications, and provide statistics.
     ● Built a mobile application to monitor a vessel, watch alert notifications independently.
@@ -21,7 +21,7 @@ export const experiences = [
     React, React Native, Gatsby, Redux, Redux Saga, D3, Google Maps, SEO, NodeJS, PostgreSQL, Sequelize, Serverless, AWS, Typescript, BLE, WebSocket, Git, Zeplin, Webpack, LESS`,
   },
   {
-    name: "Contribulet Inc., Front-end Engineer, Oct 2017 ~ Apr 2019",
+    name: "Contribulet Inc., Front-end Engineer, Oct. 2017 ~ Apr. 2019",
     location: "San Jose, U.S.A (Remote)",
     desc: `● Built the corporate website to raise charity events, participate events, and sell/purchase/redeem vouchers.
     ● Developed the management dashboard for fundraiser and merchants to organize events independently.
@@ -31,7 +31,7 @@ export const experiences = [
     React, Redux, Angular, RxJS, Cordova, Android, Nginx, PHP, SEO, Push Notification, Google Maps, Git, Zeplin, Webpack, SASS`,
   },
   {
-    name: "Dynacolor Inc, Software Engineer, Mar 2017 ~ Sep 2017",
+    name: "Dynacolor Inc, Software Engineer, Mar. 2017 ~ Sep. 2017",
     location: "Taipei, Taiwan",
     desc: "● Developed a streaming server to encode and wrap video and audio and stream to RTSP client.",
     subDesc: `Skills:
