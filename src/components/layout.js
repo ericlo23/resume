a7d5141a085ec0eb07511a240ff1029d77c3abf0
Eric Lo <eric.lo@tranx.io>
2024-06-08 01:12:48 +0800

    feat: improve SEO

--- src/components/layout.js
+++ src/components/layout.js
@@ -16,6 +16,16 @@ const Layout = ({ children }) => {
   return (
     <>
       <Helmet>
+        <title>羅元鴻 Eric Lo - Resume</title>
+        <meta
+          name="description"
+          content="Eric Lo, a software and AI engineer from Taiwan, with extensive experience in front-end development and AI application development."
+        />
+        <meta
+          name="keywords"
+          content="Eric Lo, software engineer, AI engineer, front-end development, JavaScript, React, Angular, AI development, resume"
+        />
+        <meta name="author" content="Eric Lo" />
         <link
           href="https://fonts.googleapis.com/css?family=Noto+Sans+TC&display=swap"
           rel="stylesheet"
