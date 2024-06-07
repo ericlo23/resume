43aec6c800cc5bdcc571dca3ed5ef202a9d1b21e
Eric Lo <eric.lo@tranx.io>
2024-06-07 23:58:32 +0800

    feat: update content

--- src/contents/education.js
+++ src/contents/education.js
@@ -1,10 +1,10 @@
 export const education = [
   {
-    name: "National Taiwan University",
-    desc: "Master degree, Electrical Engineering, 2012 ~ 2015",
+    name: "National Taiwan University, 2012 ~ 2015",
+    desc: "Master degree, Electrical Engineering",
   },
   {
-    name: "National Chiao Tung University",
-    desc: "Bachelor degree, Computer Science, 2007 ~ 2012",
+    name: "National Chiao Tung University, 2007 ~ 2012",
+    desc: "Bachelor degree, Computer Science",
   },
 ];
--- src/contents/experiences.js
+++ src/contents/experiences.js
@@ -1,53 +1,66 @@
 export const experiences = [
   {
-    name: "TranX.io Co. Ltd., Tech Lead, Jul. 2022 ~",
+    name: "TranX.io Co. Ltd., Technical Lead, Jul. 2022 ~ Now",
     location: "Taipei, Taiwan",
-    desc: `● Develop the data cloud platform.`,
+    desc: `Responsible for company-wide technical decisions and development.
+    ● Developed a data platform to help enterprises analyze data. 
+    ● Designed and developed a pipeline for continuously training and deploying machine learning services.
+    ● Designed and developed a generative AI chatbot for data analysis.
+    ● Designed and automated data extraction pipelines for enterprises.
+    ● Conducted requirement interviews with enterprises.
+    ● Mentored junior colleagues.`,
     subDesc: `Skills:
-    React, NodeJS, GraphQL, AWS, Git, Figma`,
+    Web Development, AI Application Development, React, NodeJS, AWS, MQTT`,
   },
   {
     name: "aetherAI Co. Ltd., Front-end Team Lead, Feb. 2022 ~ Jul. 2022",
     location: "Taipei, Taiwan",
-    desc: `● Managed the front-end team.
-    ● Participated in planning half-annual goals.
-    ● Hosted the front-end study group.`,
+    desc: `Responsible for front-end team-wide decisions and development.
+    ● Led a front-end team of 5 members.
+    ● Handled cross-team communication.
+    ● Participated in planning semi-annual goals.
+    ● Hosted a weekly front-end study group.`,
   },
   {
     name: "aetherAI Co. Ltd., Front-end Engineer, Apr. 2020 ~ Feb. 2022",
     location: "Taipei, Taiwan",
-    desc: `● Developed the front-end of digital pathology and medical imaging system.
-    ● Developed the viewer of low-dose CT images.
-    ● Developed the front-end of the digital pathology platform.
-    ● Initiated the UI library.
+    desc: `Responsible for front-end development.
+    ● Developed the front-end for a digital pathology and medical imaging system.
+    ● Developed the front-end for a viewer of low-dose CT images.
+    ● Developed the front-end for a digital pathology platform used in education and conferences.
+    ● Initiated a company-wide UI library.
     ● Mentored junior colleagues.`,
     subDesc: `Skills:
-    Angular, Three.js, GraphQL, Git, Figma`,
+    Web Development, Angular, Three.js`,
   },
   {
     name: "Arasens Inc., Front-end Engineer, Apr. 2019 ~ Oct. 2019",
     location: "Taipei, Taiwan",
-    desc: `● Developed a service platform to monitor vessels, schedule events, configure alerts, and provide statistics.
-    ● Built a mobile application to monitor a vessel and watch alerts independently.
-    ● Built the corporate website from scratch.
-    ● Maintain back-end services.`,
+    desc: `Responsible for front-end development.
+    ● Developed a platform to monitor vessels, schedule events, configure alerts, and provide statistics.
+    ● Developed a mobile application to monitor vessels and receive alerts.
+    ● Developed a corporate website from scratch.
+    ● Maintained back-end services.`,
     subDesc: `Skills:
-    React, React Native, Webpack, NodeJS, PostgreSQL, AWS, Git, Zeplin`,
+    Web Development, React, React Native, NodeJS, AWS`,
   },
   {
     name: "Contribulet Inc., Front-end Engineer, Oct. 2017 ~ Apr. 2019",
-    location: "San Jose, U.S.A (Remote)",
-    desc: `● Developed the service platform to raise charity events and sell vouchers.
-    ● Developed the management dashboard for users to organize events.
-    ● Built a mobile application for donors to purchase vouchers from scratch.
-    ● Built a POS application for merchants to redeem vouchers from scratch.`,
+    location: "San Jose, USA (Remote)",
+    desc: `Responsible for front-end and mobile development.
+    ● Developed a platform for external users to participate in charity events and purchase vouchers.
+    ● Refactored the platform from a static website to a modern web application.
+    ● Developed a mobile application from scratch for external users with the same functions as the platform.
+    ● Developed a management dashboard for internal users to organize charity events and list vouchers.
+    ● Developed a POS application from scratch for merchants to redeem vouchers.`,
     subDesc: `Skills:
-    React, Angular, Cordova, Webpack, Git, Zeplin`,
+    Web Development, React, Angular, Cordova`,
   },
   {
-    name: "Dynacolor Inc, Software Engineer, Mar. 2017 ~ Sep. 2017",
+    name: "Dynacolor Inc., Software Engineer, Mar. 2017 ~ Sep. 2017",
     location: "Taipei, Taiwan",
-    desc: "● Developed a streaming server to encode media and stream to RTSP client.",
+    desc: `Responsible for implementing well-defined tasks.
+    ● Developed a streaming server to encode media and stream to RTSP clients.`,
     subDesc: `Skills:
     C++, Streaming, RTSP, Linux`,
   },
