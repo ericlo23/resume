197aa48a1f03f918c6e5963a1f2079479c0dc8cc
Eric Lo <eric.lo@tranx.io>
2024-06-08 00:49:39 +0800

    feat: update content

--- src/contents/education.js
+++ src/contents/education.js
@@ -1,10 +1,14 @@
 export const education = [
   {
-    name: "National Taiwan University, 2012 ~ 2015",
+    name: "National Taiwan University",
+    period: "2012 ~ 2015",
+    location: "Taipei, Taiwan",
     desc: "Master degree, Electrical Engineering",
   },
   {
-    name: "National Chiao Tung University, 2007 ~ 2012",
+    name: "National Chiao Tung University",
+    period: "2007 ~ 2012",
+    location: "Hsinchu, Taiwan",
     desc: "Bachelor degree, Computer Science",
   },
 ];
--- src/contents/experiences.js
+++ src/contents/experiences.js
@@ -1,67 +1,70 @@
 export const experiences = [
   {
-    name: "TranX.io Co. Ltd., Technical Lead, Jul. 2022 ~ Now",
+    name: "TranX.io Co. Ltd., Technical Lead",
+    period: "Jul. 2022 ~ On-going",
     location: "Taipei, Taiwan",
     desc: `Responsible for company-wide technical decisions and development.
-    ● Developed a data platform to help enterprises analyze data. 
-    ● Designed and developed a pipeline for continuously training and deploying machine learning services.
-    ● Designed and developed a generative AI chatbot for data analysis.
-    ● Designed and automated data extraction pipelines for enterprises.
-    ● Conducted requirement interviews with enterprises.
-    ● Mentored junior colleagues.`,
-    subDesc: `Skills:
-    Web Development, AI Application Development, React, NodeJS, AWS, MQTT`,
+    • Participated in developing product strategies.
+    • Conducted requirement interviews with enterprises.
+    • Developed a data platform to help enterprises analyze data. 
+    • Designed and developed a pipeline for continuously training and deploying machine learning services.
+    • Designed and developed a generative AI chatbot for data analysis.
+    • Designed and automated data extraction pipelines for enterprises.
+    • Mentored junior colleagues and maintained codebase quality.
+    • Automated infrastructure deployment.`,
+    subDesc: `AI Application Development, Web Development, React, NodeJS, AWS, MQTT`,
   },
   {
-    name: "aetherAI Co. Ltd., Front-end Team Lead, Feb. 2022 ~ Jul. 2022",
+    name: "aetherAI Co. Ltd., Front-end Team Lead",
+    period: "Feb. 2022 ~ Jul. 2022",
     location: "Taipei, Taiwan",
-    desc: `Responsible for front-end team-wide decisions and development.
-    ● Led a front-end team of 5 members.
-    ● Handled cross-team communication.
-    ● Participated in planning semi-annual goals.
-    ● Hosted a weekly front-end study group.`,
+    desc: `Responsible for front-end team-wide decisions and management.
+    • Led a front-end team of 5 members.
+    • Participated in planning semi-annual goals.
+    • Handled cross-team communication.
+    • Hosted a weekly front-end study group.
+    • Continued jobs as a front-end engineer.`,
   },
   {
-    name: "aetherAI Co. Ltd., Front-end Engineer, Apr. 2020 ~ Feb. 2022",
-    location: "Taipei, Taiwan",
-    desc: `Responsible for front-end development.
-    ● Developed the front-end for a digital pathology and medical imaging system.
-    ● Developed the front-end for a viewer of low-dose CT images.
-    ● Developed the front-end for a digital pathology platform used in education and conferences.
-    ● Initiated a company-wide UI library.
-    ● Mentored junior colleagues.`,
-    subDesc: `Skills:
-    Web Development, Angular, Three.js`,
-  },
-  {
-    name: "Arasens Inc., Front-end Engineer, Apr. 2019 ~ Oct. 2019",
+    name: "aetherAI Co. Ltd., Front-end Engineer",
+    period: "Apr. 2020 ~ Feb. 2022",
     location: "Taipei, Taiwan",
     desc: `Responsible for front-end development.
-    ● Developed a platform to monitor vessels, schedule events, configure alerts, and provide statistics.
-    ● Developed a mobile application to monitor vessels and receive alerts.
-    ● Developed a corporate website from scratch.
-    ● Maintained back-end services.`,
-    subDesc: `Skills:
-    Web Development, React, React Native, NodeJS, AWS`,
+    • Developed the front-end for a digital pathology and medical imaging system.
+    • Developed the front-end for a viewer of low-dose CT images.
+    • Developed the front-end for a digital pathology platform used in education and conferences.
+    • Initiated a company-wide UI library.
+    • Mentored junior colleagues and maintained codebase quality.`,
+    subDesc: `Web Development, Angular, Three.js`,
   },
   {
-    name: "Contribulet Inc., Front-end Engineer, Oct. 2017 ~ Apr. 2019",
+    name: "Arasens Inc., Front-end Engineer",
+    period: "Apr. 2019 ~ Oct. 2019",
+    location: "Taipei, Taiwan",
+    desc: `Responsible for front-end development.
+    • Developed a platform to monitor vessels, schedule events, configure alerts, and provide statistics.
+    • Developed a mobile application to monitor vessels and receive alerts.
+    • Developed a corporate website from scratch.`,
+    subDesc: `Web Development, React, React Native`,
+  },
+  {
+    name: "Contribulet Inc., Front-end Engineer",
+    period: "Oct. 2017 ~ Apr. 2019",
     location: "San Jose, USA (Remote)",
     desc: `Responsible for front-end and mobile development.
-    ● Developed a platform for external users to participate in charity events and purchase vouchers.
-    ● Refactored the platform from a static website to a modern web application.
-    ● Developed a mobile application from scratch for external users with the same functions as the platform.
-    ● Developed a management dashboard for internal users to organize charity events and list vouchers.
-    ● Developed a POS application from scratch for merchants to redeem vouchers.`,
-    subDesc: `Skills:
-    Web Development, React, Angular, Cordova`,
+    • Developed a platform for external users to participate in charity events and purchase vouchers.
+    • Refactored the platform from a static website to a modern web application.
+    • Developed a mobile application from scratch for external users with the same functions as the platform.
+    • Developed a management dashboard for internal users to organize charity events and list vouchers.
+    • Developed a POS application from scratch for merchants to redeem vouchers.`,
+    subDesc: `Web Development, React, Angular, Cordova`,
   },
   {
-    name: "Dynacolor Inc., Software Engineer, Mar. 2017 ~ Sep. 2017",
+    name: "Dynacolor Inc., Software Engineer",
+    period: "Mar. 2017 ~ Sep. 2017",
     location: "Taipei, Taiwan",
     desc: `Responsible for implementing well-defined tasks.
-    ● Developed a streaming server to encode media and stream to RTSP clients.`,
-    subDesc: `Skills:
-    C++, Streaming, RTSP, Linux`,
+    • Developed a streaming server to encode media and stream to RTSP clients.`,
+    subDesc: `C++, Streaming, RTSP, Linux`,
   },
 ];
